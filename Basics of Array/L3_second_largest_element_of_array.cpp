fc69f8398c19e173fcfb99351506a8bf2b4ca073
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-07-30 01:15:58 +0530

    updated: finding second largest element of array

---
+++ Basics of Array/L3_second_largest_element_of_array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int second_largest(int arr[],int n){
+    int largest=arr[0];
+    int second_largest=-1;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            second_largest=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]<=largest && arr[i]>second_largest ){
+            second_largest=arr[i];
+        }
+    }
+    return second_largest;
+}
+int main(){
+    int arr[] = {12,2,1,34,21,4,7,9};
+    cout<<"second largest element ="<<second_largest(arr,8);
+    return 0;
+}

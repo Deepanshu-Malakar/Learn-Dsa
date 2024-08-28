b1c87292065fb3ecbf71436812b7e4e6e24731b6
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-08-29 00:38:40 +0530

    Create p1_best_time_to_buy_sell_stock.cpp

---
+++ Buy and sell stocks/p1_best_time_to_buy_sell_stock.cpp
@@ -0,0 +1,14 @@
+// problem statement
+// You are given an integer array which contains price of stocks on ith day
+// find the maximum profit that you can _make
+
+// example:
+// a={4,2,3,5,6,4,5}
+// buy=2 sell = 6  profit=6-2=4
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+}

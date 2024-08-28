1f2e299e69e9826cca80bb8ef0265fa44604c909
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-08-29 01:11:35 +0530

    reduced the timecomplexity of this algo to O(n2)

--- Buy and sell stocks/p1_best_time_to_buy_sell_stock.cpp
+++ Buy and sell stocks/p1_best_time_to_buy_sell_stock.cpp
@@ -6,9 +6,33 @@
 // a={4,2,3,5,6,4,5}
 // buy=2 sell = 6  profit=6-2=4
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
 
+
+int main()
+{
+
+    int a[] = {4, 1, 3, 5, 6, 4, 1};
+    int n = sizeof(a) / sizeof(int);
+
+    int max_profit = 0;
+
+    int least_price = a[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i - 1] < least_price)
+        {
+            least_price = a[i - 1];
+        }
+
+        int profit = a[i] - least_price;
+        if (profit > max_profit)
+        {
+            max_profit = profit;
+        }
+    }
+    cout << "max profit is: " << max_profit;
 }

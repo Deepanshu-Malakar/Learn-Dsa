6743a9f7cd3c044a94f3b9eb8aaa6cfb10f361a9
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-09-02 17:32:52 +0530

    Update L19_next_permutation.cpp

--- Basics of Array/L19_next_permutation.cpp
+++ Basics of Array/L19_next_permutation.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // problem statement
 //  you are given an integer array
 //  find the next permutation of the array
@@ -58,22 +57,23 @@ void test(int a[], int n)
     cout << "Test : ";
     show_arr(a, n);
     cout << "Result : ";
-    next_permutation(a,n);
+    next_permutation(a, n);
     show_arr(a, n);
     cout << "--------------" << endl;
 }
+
 int main()
 {
     cout << "--------------" << endl;
     int a[] = {1, 3, 5, 4, 3, 2};
     int n1 = sizeof(a) / sizeof(int);
-    int b[] = {1,2,1,2};
+    int b[] = {1, 2, 1, 2};
     int n2 = sizeof(b) / sizeof(int);
-    int c[] = {1,3,4,3,2};
+    int c[] = {1, 3, 4, 3, 2};
     int n3 = sizeof(c) / sizeof(int);
 
-    test(a,n1);
-    test(b,n2);
-    test(c,n3);
+    test(a, n1);
+    test(b, n2);
+    test(c, n3);
     return 0;
 }

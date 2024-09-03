4f48c551f81b482e93c5325b65f260a0cec71423
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2024-09-04 00:23:42 +0530

    updated bubble_sort

--- Sorting Algorythms/L2_bubble_sort.cpp
+++ Sorting Algorythms/L2_bubble_sort.cpp
@@ -1,32 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//theory
+// theory
 
-//a={23,3,4,32,21}
-//compare the first 2 adjacent elements and swap them if they are not sorted
+// a={23,3,4,32,21}
+// compare the first 2 adjacent elements and swap them if they are not sorted
 
+// step1==>{3,23,4,32,21}==>{3,4,23,32,21}==>{3,4,23,32,21}==>{3,4,23,21,32}   biggest element comes at last place
 
-//step1==>{3,23,4,32,21}==>{3,4,23,32,21}==>{3,4,23,32,21}==>{3,4,23,21,32}   biggest element comes at last place
+// step2==>{3,4,23,21,32}==>{3,4,23,21,32}==>{3,4,21,23,32}==>{3,4,21,23,32}
 
-//step2==>{3,4,23,21,32}==>{3,4,23,21,32}==>{3,4,21,23,32}==>{3,4,21,23,32}
+// time complexcity = O(n^2)
 
+int main()
+{
+    vector<int> v = {23, 3, 4, 32, 21};
 
-//time complexcity = O(n^2)
-
-int main(){
-    vector<int> v={23,3,4,32,21};
-
-    for(int i=0;i<4;i++){
-        for(int j=0;j<5-i-1;j++){
-            if(v[j]>v[j+1]){
-                int temp=v[j+1];
-                v[j+1]=v[j];
-                v[j]=temp;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 5 - i - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                int temp = v[j + 1];
+                v[j + 1] = v[j];
+                v[j] = temp;
             }
         }
     }
 
-    for(auto it:v){
-        cout<<it<<" ,";
+    for (auto it : v)
+    {
+        cout << it << " ,";
     }
 }

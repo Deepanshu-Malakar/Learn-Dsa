3747deee4dd9f150d76f1309269033ba923e1a01
Deepanshu-Malakar <159002536+Deepanshu-Malakar@users.noreply.github.com>
2025-01-31 01:29:00 +0530

    find min element

---
+++ Binary Search/07_minimum_in_rotated_sorted_array.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMin(int array[],int size){
+    int low=0;
+    int high=size-1;
+    int mid=(high+low)/2;
+    int min=array[mid];
+    while (low<=high)
+    {
+        mid=(high+low)/2;
+        if(array[mid]<min){
+            min=array[mid];
+        }
+        if(array[low]==array[mid] && array[mid]==array[high]){
+            low++;
+            high--;
+        }
+        else if(array[low]<=array[mid]){//left half is sorted
+            if(array[low]<min) min=array[mid];
+            low=mid+1;
+        }
+        else{//right half is sorted
+            if(array[mid]<min) min=array[mid];
+            high=mid-1;
+        }
+
+    }
+    return min;
+    
+}
+
+int main(){          //l  m   h
+    int array[]={26,26,81,1,2,5,7,9,24,25,26,26,26};
+    int size=sizeof(array)/sizeof(int);
+    int ele=26;
+    cout<<"minimum element = "<<findMin(array,size);
+}
